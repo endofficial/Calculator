ee3ee8b99548240ccf56f455fd723188418e3468
Andrea Verdirrame <andreaverdirrame11@gmail.com>
2025-12-28 11:58:09 +0100

    #I added the Calculator() method and I have

--- CalculatorLibrary/CalculatorLibrary.cs
+++ CalculatorLibrary/CalculatorLibrary.cs
@@ -1,7 +1,20 @@
-﻿namespace CalculatorLibrary
+﻿using System.Diagnostics;
+
+namespace CalculatorLibrary
 {
     public class Calculator
     {
+        public Calculator()
+        {
+            StreamWriter logFile = File.CreateText("calculator.log");
+            Trace.Listeners.Add(new TextWriterTraceListener(logFile)); // ottieni la raccolta di listener che esegue il monitoraggio dell'output di traccia
+            Trace.AutoFlush = true; // indica se chiamare il metodo flush() sulla proprietà listeners dopo ogni operazione di scrittura
+            Trace.WriteLine("Starting Calculator Log");
+
+            Trace.WriteLine(String.Format("Started {0}", System.DateTime.Now.ToString()));
+            Trace.WriteLine("Started {0}", System.DateTime.Now.ToString());
+        }
+
         public static double DoOperation(double num1, double num2, string op)
         {
             double result = double.NaN; // Default value is "not-a-number"
